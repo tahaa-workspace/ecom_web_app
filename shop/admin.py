d4b38a4fbef84890ba42f4f86f22968f4ad080d0
Tahaa Mandsorwala <tahaa.workspace@gmail.com>
2025-08-13 14:54:33 +0530

    customizing admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -3,5 +3,21 @@ from .models import Products
 from .models import Order
 # Register your models here.
 
-admin.site.register(Products)
+admin.site.site_header = "E-commerce Site"
+admin.site.site_title = "ABC shopping"
+admin.site.index_title = "Manage ABC shopping"
+
+class ProductAdmin(admin.ModelAdmin):
+    
+    def change_category_to_default(self, request, queryset):
+        queryset.update(category="default")
+        
+    change_category_to_default.short_description = 'Default Category'
+    list_display = ('title', 'price', 'discount_price', 'category', 'description')
+    search_fields = ('title', 'category')
+    actions = ('change_category_to_default', )
+    fields = ('title', 'price')
+    list_editable = ('price', 'category', )
+
+admin.site.register(Products, ProductAdmin)
 admin.site.register(Order)
